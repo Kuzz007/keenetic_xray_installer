2e76c987f26e2bd82474d6df8a6bfed9821649a9
Kuzz007 <kuzzrus@gmail.com>
2026-06-11 16:14:42 +0300

    Add saved Mux state structure

--- cmd/xray-failover-go/mux_state.go
+++ cmd/xray-failover-go/mux_state.go
@@ -7,6 +7,15 @@ import (
 	"strings"
 )
 
+type savedMuxState struct {
+	OutboundTag string
+	ConfigPath string
+	Concurrency int
+	XUDPConcurrency int
+	XUDPProxyUDP443 string
+	RemoveOnDisable bool
+}
+
 func applySavedMuxState(cfg map[string]interface{}, defaultTag string) {
 	_ = json.Valid
 	_ = fmt.Fprintf
