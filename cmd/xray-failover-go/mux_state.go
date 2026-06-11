74332966c5f63516efeb5b2881119c38a17d3985
Kuzz007 <kuzzrus@gmail.com>
2026-06-11 16:14:23 +0300

    Add imports for saved Mux state helper

--- cmd/xray-failover-go/mux_state.go
+++ cmd/xray-failover-go/mux_state.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+)
+
 func applySavedMuxState(cfg map[string]interface{}, defaultTag string) {
+	_ = json.Valid
+	_ = fmt.Fprintf
+	_ = os.Stderr
+	_ = strings.TrimSpace
 }
