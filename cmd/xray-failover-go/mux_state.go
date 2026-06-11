756ca9ecbe3a54ee4f9988280c26d9fccc8609df
Kuzz007 <kuzzrus@gmail.com>
2026-06-11 16:15:15 +0300

    Read saved Mux state path

--- cmd/xray-failover-go/mux_state.go
+++ cmd/xray-failover-go/mux_state.go
@@ -7,18 +7,25 @@ import (
 	"strings"
 )
 
-type savedMuxState struct {
-	OutboundTag string
-	ConfigPath string
-	Concurrency int
-	XUDPConcurrency int
-	XUDPProxyUDP443 string
-	RemoveOnDisable bool
-}
+const savedMuxStatePath = "/opt/etc/xray/mux-state.json"
 
 func applySavedMuxState(cfg map[string]interface{}, defaultTag string) {
+	data, err := os.ReadFile(savedMuxStatePath)
+	if err != nil || len(data) == 0 {
+		return
+	}
+	_ = data
+	_ = cfg
+	_ = defaultTag
 	_ = json.Valid
 	_ = fmt.Fprintf
 	_ = os.Stderr
 	_ = strings.TrimSpace
+}
+
+func stringMapValue(m map[string]interface{}, key string) string {
+	if v, ok := m[key].(string); ok {
+		return v
+	}
+	return ""
 }
